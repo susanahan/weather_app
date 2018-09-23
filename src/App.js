271feb134bf14f9c78d6e3436d36ed0612842b06
C4Q <c4q@Shans-MacBook-Pro.local>
2018-09-23 17:10:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     this.handleToggle();
   }
 
-
+  
   render() {
     const {info, toggle} = this.state;
     const textDisplay =  toggle ? 'Show Celsius' : 'Show Fahrenheit'
